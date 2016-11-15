2f94162bd39515ac712888ee8de2a433d593a08d
dennellmarie <dennell.lucero@gmail.com>
2016-11-15 11:56:35 -0800

    minor changes to timer, added ==null to server/index

--- client/js/components/Timer.js
+++ client/js/components/Timer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import actions from './actions/actions';
+import actions from '../actions/actions';
 
 // const Timer = () => {
 // 	return (
