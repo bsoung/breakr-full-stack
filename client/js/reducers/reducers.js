72c0850a6638bb89c3f7568453ecef346d816169
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-17 00:16:23 -0500

    more code cleanup

--- client/js/reducers/reducers.js
+++ client/js/reducers/reducers.js
@@ -1,20 +1,12 @@
 import { WORK_TIMER_START, BREAK_TIMER_START, SET_USER } from '../actions/actions';
 
 const initialState = {
-	selectedImage: null,
-	checkedOptions: [],
-	account: null,
-	selectedMusic: null,
-	selectedTime: 0,
-	start: false,
 	workTimeRemaining: null,
 	breakTimeRemaining: null,
 	user: null
 };
 
 export default function (state=initialState, action={}) {
-	// var copyState = state || initialState;
-	// state = Object.assign({}, copyState);
 	const payload = {};
 
 	switch (action.type) {
