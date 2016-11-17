d6ced78dc2c024de9d4725d447b2b77ad09c4aaa
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-17 12:31:33 -0500

    add more routing and profile page

--- client/js/components/CreateAccount.js
+++ client/js/components/CreateAccount.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createUser } from '../actions/actions';
 import { Link } from 'react-router';
---
+++ client/js/components/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+
+class Profile extends Component {
+	render () {
+		return (
+			<div>
+				This is the user profile page!
+			</div>
+		)
+	}
+}
+
+export default Profile;
