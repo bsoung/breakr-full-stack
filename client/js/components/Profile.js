225a5d9d806dd8d76079c3f69ea630b4e195185f
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-17 13:00:10 -0500

    add return button to profile

--- client/js/components/Profile.js
+++ client/js/components/Profile.js
@@ -4,9 +4,13 @@ import { connect } from 'react-redux';
 
 class Profile extends Component {
 	render () {
+		
 		return (
 			<div>
 				This is the user profile page!
+				<Link to='/timer' >
+					<button className='btn btn-primary'>Back</button>
+				</Link>
 			</div>
 		)
 	}
